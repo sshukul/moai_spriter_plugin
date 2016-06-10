e8b7a9b36e7309b733f144eb61cbd8b4d14a2066
Saurabh Shukul <saurabhshukul83@gmail.com>
2016-06-10 08:10:04 +0800

    removed unnecessary alpha flags

--- spriter.lua
+++ spriter.lua
@@ -63,7 +63,7 @@ function comparePropPriorities(a,b)
   end
 end
 
-local function createAnim ( self, name, x, y, scaleX, scaleY, reverseFlag, noSound, noAlpha )
+local function createAnim ( self, name, x, y, scaleX, scaleY, reverseFlag, noSound )
   local layerSize = 12;
   if noAlpha then
     layerSize = 8
@@ -74,13 +74,6 @@ local function createAnim ( self, name, x, y, scaleX, scaleY, reverseFlag, noSou
   
   local root = MOAITransform.new ()
   local props = {}
-  
-  local hasAlpha = false
-  for i, curveSet in orderedPairs ( self.curves[name] ) do
-    if curveSet.a ~= nil and curveSet.a ~= 1.0 then 
-      hasAlpha = true
-    end
-  end
 
   for i, curveSet in orderedPairs ( self.curves[name] ) do
     local prop = MOAIProp2D.new ()
@@ -107,18 +100,12 @@ local function createAnim ( self, name, x, y, scaleX, scaleY, reverseFlag, noSou
     player:setLink ( c + 7, curveSet.px, prop, MOAITransform.ATTR_X_PIV )
     player:setLink ( c + 8, curveSet.py, prop, MOAITransform.ATTR_Y_PIV )
     
-    -- Use the noAlpha flag for sprites where you are manipulating the color 
-    -- manually, for example setting color black for shadows, as premultiplied
-    -- alpha manipulation will overrwrite that. hasAlpha detects if a sprite
-    -- has alpha changes or not and skips alpha manipulation in those cases
-    if (noAlpha == nil or noAlpha == false) and hasAlpha then
-      -- Moai uses premultiplied alpha, 
-      -- so we should multiply every color component by alpha value
-      player:setLink ( c + 9, curveSet.a, prop, MOAIColor.ATTR_A_COL )
-      player:setLink ( c + 10, curveSet.a, prop, MOAIColor.ATTR_B_COL )
-      player:setLink ( c + 11, curveSet.a, prop, MOAIColor.ATTR_G_COL )
-      player:setLink ( c + 12, curveSet.a, prop, MOAIColor.ATTR_R_COL )
-    end
+    -- Moai uses premultiplied alpha, 
+    -- so we should multiply every color component by alpha value
+    player:setLink ( c + 9, curveSet.a, prop, MOAIColor.ATTR_A_COL )
+    player:setLink ( c + 10, curveSet.a, prop, MOAIColor.ATTR_B_COL )
+    player:setLink ( c + 11, curveSet.a, prop, MOAIColor.ATTR_G_COL )
+    player:setLink ( c + 12, curveSet.a, prop, MOAIColor.ATTR_R_COL )
     
     player:setCurve(curveSet.id)
     table.insert ( props, i, prop )
